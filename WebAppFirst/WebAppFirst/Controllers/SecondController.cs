adceb1e60bf44f242582b411bde22c9f799e72fc
itiscs <moudroff@gmail.com>
2024-11-28 11:10:55 +0300

    MVC first

---
+++ WebAppFirst/WebAppFirst/Controllers/SecondController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppFirst.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppFirst/WebAppFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
