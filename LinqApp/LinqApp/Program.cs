93aa4028efad71455fdcd9b307d4e567861a6289
itiscs <moudroff@gmail.com>
2025-02-19 10:38:37 +0300

    Linq

---
+++ LinqApp/LinqApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LinqApp;
+
+var studs = Student.Generate();
+
+
+//var expr = studs.Where(s => s.Group == "6310").TakeLast(3);
+
+//var expr = studs.OrderByDescending(s => s.Group)
+//    .ThenBy(s => s.Marks.Average());
+
+//var expr = studs.Where(s=>s.Group=="6310").SelectMany(s => s.Marks);
+
+//Console.WriteLine(studs.OrderByDescending(s => s.Marks.Average()).Last());
+
+//Console.WriteLine(studs.FirstOrDefault(s => s.Group == "6312"));
+
+
+//var expr = studs.Select(s => new { s.Group, s.Name, Len = s.Name.Length });
+
+
+var expr = studs.GroupBy(s => s.Group)
+    .Select(g=>new { Gr = g.Key, Count = g.Count(), 
+        max = g.Max(s=> s.Marks.Average()) }) ;
+
+
+foreach (var st in expr)
+{
+    Console.WriteLine(st);
+}
---
+++ LinqApp/LinqApp/Student.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqApp
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+
+        public List<int> Marks { get; set; } = new List<int>();
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Group} {Marks.Average()}";
+        }
+
+
+        public static List<Student> Generate()
+        {
+            var students = new List<Student>();
+            students.Add(new Student
+            {
+                Id = 1,
+                Name = "Ivan",
+                Group = "6310",
+                Marks = new List<int>() { 5, 5, 5, 5, 4, 4, 4 }
+            });
+            students.Add(new Student
+            {
+                Id = 2,
+                Name = "Petr",
+                Group = "6310",
+                Marks = new List<int>() { 5, 3, 3, 3, 4, 4, 4 }
+            });
+
+            students.Add(new Student
+            {
+                Id = 3,
+                Name = "Andrej",
+                Group = "6310",
+                Marks = new List<int>() { 3, 3, 3, 3, 3, 3, 3, 5 }
+            });
+
+            students.Add(new Student
+            {
+                Id = 4,
+                Name = "Ivan 4",
+                Group = "6310",
+                Marks = new List<int>() { 5, 5, 5, 5}
+            });
+
+            students.Add(new Student
+            {
+                Id = 5,
+                Name = "Ivan 5",
+                Group = "6310",
+                Marks = new List<int>() {5, 4, 4, 4 }
+            });
+
+            students.Add(new Student
+            {
+                Id = 6,
+                Name = "Ivan 6",
+                Group = "6311",
+                Marks = new List<int>() { 2,2,2,2, 4, 4, 4 }
+            });
+
+            students.Add(new Student
+            {
+                Id = 7,
+                Name = "Ivan 7",
+                Group = "6311",
+                Marks = new List<int>() { 3,3,3,3,3,3 }
+            });
+
+            students.Add(new Student
+            {
+                Id = 8,
+                Name = "Ivan 8 ",
+                Group = "6311",
+                Marks = new List<int>() { 5, 5, 5, 5, 4, 4, 4 }
+            });
+
+            return students;
+        }
+           
+    }
+}
